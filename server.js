0463fee51737cdbddce9f26c80d7cccbef6b77ee
Rohit3003T <152482885+Rohit3003T@users.noreply.github.com>
2024-04-24 18:18:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3001;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+  });
+  app.get("/about_us", (req, res) => {
+    res.render("about_us.ejs");
+  });
+  app.get("/cpf", (req, res) => {
+    res.render("cpf.ejs");
+  });
+  app.get("/committees", (req, res) => {
+    res.render("committees.ejs");
+  });
+  app.get("/keynotes", (req, res) => {
+    res.render("keynotes.ejs");
+  });
+  app.get("/submission", (req, res) => {
+    res.render("submission.ejs");
+  });
+  app.get("/contact_us", (req, res) => {
+    res.render("contact_us.ejs");
+  });
+
+  
+
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+  
